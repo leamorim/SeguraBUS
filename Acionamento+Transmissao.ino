624413d71ce81cbfcbc3df84ab03a019796df41e
leydsonbarros <leydsonbarros@users.noreply.github.com>
2017-07-13 14:48:33 -0300

    alteração no método GET

--- Acionamento+Transmissao.ino
+++ Acionamento+Transmissao.ino
@@ -10,7 +10,7 @@ char onibus[] = "7007";
 
 ///informações para conexão com o servidor
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-byte ip[] = { 10, 0, 0 , 104 };
+byte ip[] = { 10, 0, 0 , 102 };
 byte servidor[] = { 10, 0, 0, 101};
 
 EthernetClient cliente;
@@ -40,7 +40,7 @@ void setup() {
 
 
 void loop() {
-  
+
   long latitude, longitude;//variáveis do GPS
   String DatEth;//variável para interpretação da string
 
@@ -59,9 +59,9 @@ void loop() {
   Serial.println(".");
   delay(200);
   Serial.println("Conectando..");
-  
+
   if (cliente.connect(servidor, 80)) { //estado de teste de conexão
-    cliente.println("GET segurabus/PHP/wfile.php?msg=ledon");
+    cliente.println("GET /segurabus/PHP/wfile2.php?msg=ledon");
     Serial.println("Conectado");
     Serial.println("Alarme Ativado com Sucesso!");
     digitalWrite(ledPin, HIGH);   // acende o LED para indicar que a informação está sendo enviada
@@ -69,103 +69,104 @@ void loop() {
   }
   else {
     Serial.println("Sem conexao!");
-    
+
     for (int i = 0; i < 19; i++) {
       Serial.println("Tentando conectar !");
       if (cliente.connect(servidor, 80)) {
-        cliente.println("GET segurabus/PHP/wfile.php?msg=ledon");
-        Serial.println("Alarme Ativado com Sucesso!");
+        cliente.println("GET /segurabus/PHP/wfile2.php?msg=ledon");
+        Serial.println("Conectado ! Alarme Ativado com Sucesso!");
         digitalWrite(ledPin, HIGH);
         digitalWrite(ledPin2, LOW);
         conexao = true;
         break;
       }
-      else{
-        for(int i = 0; i < 8;i++){//loop apenas para passar o tempo de tentar enviar novamente
-            digitalWrite(ledPin2, HIGH);
-            delay(2000);
-            digitalWrite(ledPin2, LOW);
-            delay(2000);
+      else {
+        for (int i = 0; i < 8; i++) { //loop apenas para passar o tempo de tentar enviar novamente
+          digitalWrite(ledPin2, HIGH);
+          delay(2000);
+          digitalWrite(ledPin2, LOW);
+          delay(2000);
         }
+        conexao = false;
       }
     }
-    conexao = false;
+
   }
 
   cliente.stop();
 
-  if(conexao){
+  if (conexao == true) {
 
-  Serial.println("Enviando a informacao");
-  //Aqui seria onde esperaríamos o sinal vindo do monitoramento para desativar o alerta
-  while (!desativador == 0) {
-    //Aqui deveria enviar a informação e esperar o sinal para desativar o alarme
-    if (cliente.connect(servidor, 80)) {//talvez n precise
-      Serial.println("conectado");
+    Serial.println("Enviando a informacao");
+    //Aqui seria onde esperaríamos o sinal vindo do monitoramento para desativar o alerta
+    while (!desativador == 0) {
+      //Aqui deveria enviar a informação e esperar o sinal para desativar o alarme
+      if (cliente.connect(servidor, 80)) {//talvez n precise
+        Serial.println("conectado");
 
-      // Make a HTTP request:
-      cliente.println("GET segurabus/PHP/volta.php");
-      delay(600);
-      while (cliente.available()) {
-        // Serial.println("WHILE");
-        char c = cliente.read();
-        Serial.print(c);
-        DatEth.concat(c);
-        if (DatEth.endsWith("/ledon")) {
-          desativador = 1;
-          break;
+        // Make a HTTP request:
+        cliente.println("GET /segurabus/PHP/volta.php");
+        delay(700);
+        while (cliente.available()) {
+          // Serial.println("WHILE");
+          char c = cliente.read();
+          Serial.print(c);
+          DatEth.concat(c);
+          if (DatEth.endsWith("/ledon")) {
+            desativador = 1;
+            break;
+          }
+          if (DatEth.endsWith("/ledof")) {
+            desativador = 0;
+            break;
+          }
         }
-        if (DatEth.endsWith("/ledof")) {
-          desativador = 0;
-          break;
+        cliente.stop();
+      }
+      else {
+        Serial.println("Não conectou !");
+        cliente.stop();
+      }
+
+
+      bool recebido = false;
+      while (serial1.available()) {
+        char cIn = serial1.read();
+        recebido = gps1.encode(cIn);
+        recebido = true;
+
+      }
+
+      if (recebido) {
+        Serial.println("----------------------------------------");
+
+        //Latitude e Longitude
+        unsigned long idadeInfo;
+        gps1.get_position(&latitude, &longitude, &idadeInfo);
+
+        if (latitude != TinyGPS::GPS_INVALID_F_ANGLE) {
+          Serial.print("Latitude: ");
+          Serial.println(float(latitude) / 100000, 6);
+        }
+
+        if (longitude != TinyGPS::GPS_INVALID_F_ANGLE) {
+          Serial.print("Longitude: ");
+          Serial.println(float(longitude) / 100000, 6);
+        }
+
+        if (idadeInfo != TinyGPS::GPS_INVALID_AGE) {
+          Serial.print("Idade da Informacao (ms): ");
+          Serial.println(idadeInfo);
         }
       }
-      cliente.stop();
-    }
-    else {
-      Serial.println("Não conectou !");
-      cliente.stop();
-    }
-
-
-    bool recebido = false;
-    while (serial1.available()) {
-      char cIn = serial1.read();
-      recebido = gps1.encode(cIn);
-      recebido = true;
-
-    }
-
-    if (recebido) {
-      Serial.println("----------------------------------------");
-
-      //Latitude e Longitude
-      unsigned long idadeInfo;
-      gps1.get_position(&latitude, &longitude, &idadeInfo);
-
-      if (latitude != TinyGPS::GPS_INVALID_F_ANGLE) {
-        Serial.print("Latitude: ");
-        Serial.println(float(latitude) / 100000, 6);
+      else { //caso o GPS não esteja funcionando
+        latitude = 0.0000000;
+        longitude = 0.0000000;
       }
 
-      if (longitude != TinyGPS::GPS_INVALID_F_ANGLE) {
-        Serial.print("Longitude: ");
-        Serial.println(float(longitude) / 100000, 6);
-      }
-
-      if (idadeInfo != TinyGPS::GPS_INVALID_AGE) {
-        Serial.print("Idade da Informacao (ms): ");
-        Serial.println(idadeInfo);
-      }
-    }
-    else{//caso o GPS não esteja funcionando
-      latitude = 0.0000000;
-      longitude = 0.0000000;
-    }
-    
       if (cliente.connect(servidor, 80)) {
         // Make a HTTP request:
-        cliente.print("GET segurabus/PHP/salvardados.php?");
+        cliente.print("GET /segurabus/PHP/salvardados.php?");
         cliente.print("latitude=");
         cliente.print(float(latitude) / 100000, 6);
         cliente.print("&longitude=");
@@ -183,26 +184,24 @@ void loop() {
         Serial.println(onibus);
         Serial.print("status=");
         Serial.println(desativador);
-        
+
 
         cliente.stop();
       }
       else {
         cliente.stop();
       }
-      
-    delay(5000);//tempo de enviar outro dado
-  }
-  desativador = 1;//variável de controle do loop
-  /////Chega nessa parte quando o alarme for desativado
 
-  digitalWrite(ledPin, LOW);
-  delay(100);
-  Serial.println("Alarme Desativado !");
+      delay(5000);//tempo de enviar outro dado
+    }
+    desativador = 1;//variável de controle do loop
+    /////Chega nessa parte quando o alarme for desativado
+
+    digitalWrite(ledPin, LOW);
+    delay(100);
+    Serial.println("Alarme Desativado !");
   }
 
   //delay(1000);
 
 }
-
-
